eb0a0920822f279972782e1f7631d2681a6476e2
yesidramirez <yesidramirezm@hotmail.com>
2025-01-10 17:12:33 -0500

    mapeo y cambio de input

--- login.js
+++ login.js
@@ -1,13 +1,24 @@
+let msgError = document.querySelector("#alert p");
 var formulario = document.getElementById("forma");
+const alert = document.getElementById("alert");
+const x = document.getElementById("x");
+
+console.log(msgError);
+
+x.addEventListener("click", function (e) {
+  console.log(x);
+  alert.classList.toggle("show");
+});
+console.log(alert);
 
 formulario.addEventListener("submit", function (e) {
   e.preventDefault();
-
   var datos = new FormData(formulario);
 
   // Verifica los valores del formulario en consola
   console.log(datos.get("usuario")); // Debe imprimir el valor del campo 'usuario'
   console.log(datos.get("password")); // Debe imprimir el valor del campo 'password'
+  var usuario = datos.get("usuario");
 
   fetch("http://54.227.80.41:3000/api/v1/users/login", {
     method: "POST",
@@ -22,11 +33,31 @@ formulario.addEventListener("submit", function (e) {
     .then((res) => res.json())
     .then((data) => {
       console.log(data); // Muestra la respuesta del servidor
-       if(data.ok ===true){
-        localStorage.setItem("token",JSON.stringify(data))
-        window.location.href = "/perfil/REDIRECCION.html";
-      } 
-
+      if (data.message === "User not found") {
+        msgError.textContent =
+          "!Recorcholis¡ creo que ingrasaste un correo incorrecto ";
+        alert.classList.toggle("show");
+      } else if (data.message === "Invalid password") {
+        msgError.textContent = "tu contraseña no es correcta";
+        alert.classList.toggle("show");
+      } else if (data.ok === true) {
+        if (usuario == "davidlealpelaez@gmail.com") {
+          const token = {
+            admin: true,
+            token: data.msg,
+          };
+          localStorage.setItem("token", JSON.stringify(token));
+          window.location.href = "./perfil/REDIRECCION.html";
+        } else {
+          console.log("hasta aqui te trajo el rio");
+          const token = {
+            admin: false,
+            token: data.msg,
+          };
+          localStorage.setItem("token", JSON.stringify(token));
+          window.location.href = "./perfil/REDIRECCION.html";
+        }
+      }
     })
     .catch((error) => {
       console.log("Hubo un problema con la petición Fetch: " + error);
--- perfil/perfil.js
+++ perfil/perfil.js
@@ -1,6 +1,8 @@
 const token = JSON.parse(localStorage.getItem("token"));
 console.log(token);
+
 var container =document.getElementById("container")
+
 fetch("http://54.227.80.41:3000/api/v1/users/profile", {
   method: "GET",
   headers: {
